650c309914931f9897c829c86c88eae3a9ba7269
Hassan Ali <helfi92@gmail.com>
2019-03-27 13:31:11 -0400

    Add material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { hot } from 'react-hot-loader';
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 import Main from './Main';
-import './App.css';
 
 const App = () => (
-  <div className="App">
-    <h1>Welcome to Balrog Admin</h1>
-    <Main />
+  <div>
+    <ThemeProvider theme={theme}>
+      <Main />
+    </ThemeProvider>
   </div>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// TODO: Customize the theme
+export default createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
