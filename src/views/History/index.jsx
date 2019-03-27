ad964e1f16832c30704498547464659b377a1c10
Hassan Ali <helfi92@gmail.com>
2019-03-27 13:31:35 -0400

    Add skeleton to history page

--- src/views/History/index.jsx
+++ src/views/History/index.jsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Grid from '@material-ui/core/Grid';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import DataTable from '../../components/DataTable';
+import HistoryForm from '../../components/HistoryForm';
 
 function History() {
-  return <div>History</div>;
+  return (
+    <Fragment>
+      <Grid container>
+        <Grid item xs={12} sm={4}>
+          <HistoryForm onSubmit={() => {}} />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <DataTable
+            headers={['sno', 'Object', 'Changed By', 'Date', 'Data Version']}
+            items={[]}
+            renderRow={() => (
+              <TableRow>
+                <TableCell>1</TableCell>
+                <TableCell>2</TableCell>
+                <TableCell>3</TableCell>
+                <TableCell>4</TableCell>
+                <TableCell>5</TableCell>
+              </TableRow>
+            )}
+          />
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
 }
 
 export default History;
