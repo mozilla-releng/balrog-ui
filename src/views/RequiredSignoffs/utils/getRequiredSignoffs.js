ffbc7614daa01110ca806424479e0ef79a47d02f
Hassan Ali <helfi92@gmail.com>
2019-05-20 17:43:35 -0400

    Adjust getRequiredSignoffs so that it can also fetch a single resource

--- src/views/RequiredSignoffs/utils/getRequiredSignoffs.js
+++ src/views/RequiredSignoffs/utils/getRequiredSignoffs.js
@@ -1,11 +1,20 @@
+import { stringify } from 'qs';
 import { OBJECT_NAMES } from '../../../utils/constants';
 import RequiredSignoffs from '../ListSignoffs/RequiredSignoffs';
 import rsService from '../../../services/requiredSignoffs';
 
-// Holds ALL of the Required Signoffs - product, permissions,
-// and scheduled changes for each
-export default async () => {
+// A utlity to holds all of the Required Signoffs - product, permissions,
+// and scheduled changes
+export default async (product, channel) => {
   const requiredSignoffs = new RequiredSignoffs();
+  const withQuery = url => {
+    if (url.includes(OBJECT_NAMES.PERMISSIONS_REQUIRED_SIGNOFF)) {
+      return url + stringify({ product }, { addQueryPrefix: true });
+    }
+
+    return url + stringify({ product, channel }, { addQueryPrefix: true });
+  };
+
   const [
     {
       data: { required_signoffs: productRS },
@@ -20,10 +29,18 @@ export default async () => {
       data: { scheduled_changes: permissionsSC },
     },
   ] = await Promise.all([
-    rsService.getRequiredSignoffs(OBJECT_NAMES.PRODUCT_REQUIRED_SIGNOFF),
-    rsService.getRequiredSignoffs(OBJECT_NAMES.PERMISSIONS_REQUIRED_SIGNOFF),
-    rsService.getScheduledChanges(OBJECT_NAMES.PRODUCT_REQUIRED_SIGNOFF),
-    rsService.getScheduledChanges(OBJECT_NAMES.PERMISSIONS_REQUIRED_SIGNOFF),
+    rsService.getRequiredSignoffs(
+      withQuery(OBJECT_NAMES.PRODUCT_REQUIRED_SIGNOFF)
+    ),
+    rsService.getRequiredSignoffs(
+      withQuery(OBJECT_NAMES.PERMISSIONS_REQUIRED_SIGNOFF)
+    ),
+    rsService.getScheduledChanges(
+      withQuery(OBJECT_NAMES.PRODUCT_REQUIRED_SIGNOFF)
+    ),
+    rsService.getScheduledChanges(
+      withQuery(OBJECT_NAMES.PERMISSIONS_REQUIRED_SIGNOFF)
+    ),
   ]);
 
   requiredSignoffs.setProductRequiredSignoffs(productRS);
