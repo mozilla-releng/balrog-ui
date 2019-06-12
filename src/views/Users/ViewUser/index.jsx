0474c0576fdfe32b28ca9d405bbfb91366dc3058
Ben Hearsum <bhearsum@mozilla.com>
2019-06-12 12:08:30 -0400

    Fetch existing user data.

--- src/views/Users/ViewUser/index.jsx
+++ src/views/Users/ViewUser/index.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { bool } from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import ErrorPanel from '@mozilla-frontend-infra/components/ErrorPanel';
+import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import Dashboard from '../../../components/Dashboard';
+import useAction from '../../../hooks/useAction';
+import { getUserInfo } from '../../../utils/Users';
 
 function ViewUser({ isNewUser, ...props }) {
-  console.log(props);
+  const {
+    match: {
+      params: { username: existingUsername },
+    },
+  } = props;
+  const [username, setUsername] = useState('');
+  const [roles, setRoles] = useState({});
+  const [permissions, setPermissions] = useState({});
+  const [userAction, fetchUser] = useAction(getUserInfo);
+  const isLoading = userAction.loading;
+  const { error } = userAction;
 
-  return <Dashboard title="Users"> i am a user </Dashboard>;
+  useEffect(() => {
+    if (!isNewUser) {
+      fetchUser(existingUsername).then(result => {
+        setUsername(result.data.data.username);
+        setRoles(result.data.data.roles);
+        setPermissions(result.data.data.permissions);
+      });
+    }
+  }, []);
+
+  const handleUsernameChange = ({ target: { value } }) => setUsername(value);
+
+  return (
+    <Dashboard title="Users">
+      {error && <ErrorPanel error={error} />}
+      {isLoading && <Spinner loading />}
+      {!isLoading && (
+        <Fragment>
+          <form autoComplete="off">
+            <Grid container spacing={2}>
+              <Grid item xs>
+                <TextField
+                  required
+                  disabled={!isNewUser}
+                  onChange={handleUsernameChange}
+                  fullWidth
+                  label="Username"
+                  value={username}
+                />
+              </Grid>
+            </Grid>
+          </form>
+        </Fragment>
+      )}
+    </Dashboard>
+  );
 }
 
 ViewUser.propTypes = {
