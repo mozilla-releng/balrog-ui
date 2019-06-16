d9d399c43bcd1e5d4ea67ca30cd189783475cb57
Hassan Ali <helfi92@gmail.com>
2019-06-16 10:10:50 -0700

    Add user view and allow dot in URL

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import lazy from './utils/lazy';
 import releaseRoutes from './views/Releases/routes';
 import ruleRoutes from './views/Rules/routes';
+import userRoutes from './views/Users/routes';
 import requiredSignoffsRoutes from './views/RequiredSignoffs/routes';
 
 const History = lazy(() =>
@@ -44,6 +45,7 @@ export default [
   {
     component: Users,
     path: '/users',
+    routes: userRoutes('/users'),
   },
   {
     component: Roles,
