a32ca4a40894650a660534b3ba3b6790b46c974d
Ben Hearsum <bhearsum@mozilla.com>
2019-06-26 10:46:40 -0400

    More readable deleteRule function.

--- src/views/Rules/utils/deleteRule.jsx
+++ src/views/Rules/utils/deleteRule.jsx
@@ -4,17 +4,17 @@ import { deleteRule } from '../../../services/rules';
 export default async rule => {
   // TODO: why can't we index the result to just get the error?
   // await stops working if we do
-  const result = await tryCatch(
+  const error = (await tryCatch(
     deleteRule({
       ruleId: rule.rule_id,
       dataVersion: rule.data_version,
     })
-  );
+  ))[0];
 
-  if (result[0]) {
+  if (error) {
     const errorMsg =
-      result[0].response.data.exception ||
-      result[0].response.data.detail ||
+      error.response.data.exception ||
+      error.response.data.detail ||
       'Unknown Error';
 
     throw new Error(errorMsg);
