07b2ad60c1925c028818e0987b7e78174a8be3ae
Hassan Ali <helfi92@gmail.com>
2019-07-10 15:21:44 -0400

    Add initial view for releases

--- src/views/Releases/ListReleases/index.jsx
+++ src/views/Releases/ListReleases/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PlusIcon from 'mdi-react/PlusIcon';
 import { makeStyles } from '@material-ui/styles';
 import Fab from '@material-ui/core/Fab';
 import Tooltip from '@material-ui/core/Tooltip';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import Dashboard from '../../../components/Dashboard';
+import ReleaseCard from '../../../components/ReleaseCard';
+import useAction from '../../../hooks/useAction';
 import Link from '../../../utils/Link';
+import { getReleases } from '../../../services/releases';
 
 const useStyles = makeStyles(theme => ({
   fab: {
@@ -15,17 +19,34 @@ const useStyles = makeStyles(theme => ({
 
 function ListPermissions() {
   const classes = useStyles();
+  const [releases, fetchReleases] = useAction(getReleases);
+  const isLoading = releases.loading;
+
+  useEffect(() => {
+    fetchReleases();
+  }, []);
 
   return (
     <Dashboard title="Releases">
-      <Typography variant="subtitle1">Releases</Typography>
-      <Link to="/releases/create">
-        <Tooltip title="Add Release">
-          <Fab color="primary" className={classes.fab}>
-            <PlusIcon />
-          </Fab>
-        </Tooltip>
-      </Link>
+      {isLoading && <Spinner loading />}
+      {!isLoading && releases.data && (
+        <Grid container spacing={4}>
+          {releases.data.data.releases.map(release => (
+            <Grid key={release.name} item xs={12}>
+              <ReleaseCard release={release} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+      {!isLoading && (
+        <Link to="/releases/create">
+          <Tooltip title="Add Release">
+            <Fab color="primary" className={classes.fab}>
+              <PlusIcon />
+            </Fab>
+          </Tooltip>
+        </Link>
+      )}
     </Dashboard>
   );
 }
