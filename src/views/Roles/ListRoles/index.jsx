b1ea38ad2694b73ae01f29b77e6b56a92ae34234
Hassan Ali <helfi92@gmail.com>
2019-07-16 18:16:00 -0400

    Add ability to filter by role

--- src/views/Roles/ListRoles/index.jsx
+++ src/views/Roles/ListRoles/index.jsx
@@ -1,15 +1,18 @@
-import React, { Fragment, useEffect, useMemo } from 'react';
+import React, { Fragment, useState, useEffect, useMemo } from 'react';
 import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import { makeStyles } from '@material-ui/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 import Dashboard from '../../../components/Dashboard';
 import ErrorPanel from '../../../components/ErrorPanel';
 import useAction from '../../../hooks/useAction';
 import { getUsers } from '../../../services/users';
 
+const ALL = 'all';
 const useStyles = makeStyles(theme => ({
   title: {
     marginBottom: theme.spacing(3),
@@ -20,11 +23,20 @@ const useStyles = makeStyles(theme => ({
   roleWrapper: {
     marginBottom: theme.spacing(4),
   },
+  options: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  dropdown: {
+    minWidth: 200,
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 function ListRules() {
   const classes = useStyles();
   const [usersAction, fetchUsers] = useAction(getUsers);
+  const [roleFilter, setRoleFilter] = useState(ALL);
   const isLoading = usersAction.loading;
   const { error } = usersAction;
   const roles = useMemo(() => {
@@ -52,18 +64,41 @@ function ListRules() {
       })
     );
   }, [usersAction.data]);
+  const filteredRoles = useMemo(
+    () => (roleFilter === ALL ? roles : [roleFilter]),
+    [roles, roleFilter]
+  );
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  const handleFilterChange = ({ target: { value } }) => {
+    setRoleFilter(value);
+  };
+
   return (
     <Dashboard title="Roles">
       {isLoading && <Spinner loading />}
       {error && <ErrorPanel fixed error={error} />}
-      {roles.length && users.length && (
+      {filteredRoles.length && users.length && (
         <Fragment>
-          {roles.map(role => (
+          <div className={classes.options}>
+            <TextField
+              className={classes.dropdown}
+              select
+              label="Role"
+              value={roleFilter}
+              onChange={handleFilterChange}>
+              <MenuItem value="all">All Roles</MenuItem>
+              {roles.map(role => (
+                <MenuItem key={role} value={role}>
+                  {role}
+                </MenuItem>
+              ))}
+            </TextField>
+          </div>
+          {filteredRoles.map(role => (
             <div key={role} className={classes.roleWrapper}>
               <Typography variant="h6" className={classes.title}>
                 {role}
