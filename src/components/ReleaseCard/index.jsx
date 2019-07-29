f40c5cb8c5cbe0b41d1ea53db476ad87f0542171
Hassan Ali <helfi92@gmail.com>
2019-07-29 09:32:10 -0400

    Rename classname

--- src/components/ReleaseCard/index.jsx
+++ src/components/ReleaseCard/index.jsx
@@ -47,7 +47,7 @@ const useStyles = makeStyles(theme => ({
   cardHeaderContent: {
     ...theme.mixins.textEllipsis,
   },
-  ruleName: {
+  releaseName: {
     ...theme.mixins.textEllipsis,
   },
   listItem: {
@@ -106,7 +106,7 @@ function ReleaseCard(props) {
         className={classes.cardHeader}
         classes={{ content: classes.cardHeaderContent }}
         title={
-          <Typography className={classes.ruleName} component="h2" variant="h6">
+          <Typography className={classes.releaseName} component="h2" variant="h6">
             {release.name}{' '}
             <a
               href={`#${release.name}`}
