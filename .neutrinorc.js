4a10e3e892fcd8b1fa288b0a2221407353731753
Ben Hearsum <bhearsum@mozilla.com>
2019-08-06 13:40:55 -0400

    Fix frame-src

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -33,7 +33,7 @@ module.exports = {
                            note: this is different in stage/prod
               frame-src: https://balrog-localdev.auth0.com for background token refreshes
             */
-            'Content-Security-Policy': "default-src 'none'; script-src 'self' 'unsafe-eval'; img-src 'self' https://*.gravatar.com https://*.githubusercontent.com https://i1.wp.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; connect-src http://localhost:9000 ws://localhost:9000 https://localhost:8010 https://balrog-localdev.auth0.com; frame-src: https://balrog-localdev.auth0.com",
+            'Content-Security-Policy': "default-src 'none'; script-src 'self' 'unsafe-eval'; img-src 'self' https://*.gravatar.com https://*.githubusercontent.com https://i1.wp.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; connect-src http://localhost:9000 ws://localhost:9000 https://localhost:8010 https://balrog-localdev.auth0.com; frame-src https://balrog-localdev.auth0.com",
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
             'X-XSS-Protection': '1; mode=block',
