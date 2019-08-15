706870fc486417bc958e47ee58333947f7b5316a
Hassan Ali <helfi92@gmail.com>
2019-08-14 22:15:56 -0400

    Virtualize list of rule revisions

--- src/views/Rules/ListRuleRevisions/index.jsx
+++ src/views/Rules/ListRuleRevisions/index.jsx
@@ -1,44 +1,33 @@
 import React, { Fragment, useState, useEffect } from 'react';
+import { Column } from 'react-virtualized';
 import Spinner from '@mozilla-frontend-infra/components/Spinner';
 import { makeStyles } from '@material-ui/styles';
-import TableCell from '@material-ui/core/TableCell';
-import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
 import Drawer from '@material-ui/core/Drawer';
 import { formatDistanceStrict } from 'date-fns';
 import Dashboard from '../../../components/Dashboard';
-import DataTable from '../../../components/DataTable';
 import ErrorPanel from '../../../components/ErrorPanel';
 import RuleCard from '../../../components/RuleCard';
 import Radio from '../../../components/Radio';
 import Button from '../../../components/Button';
 import DiffRule from '../../../components/DiffRule';
+import RevisionsTable from '../../../components/RevisionsTable';
 import useAction from '../../../hooks/useAction';
 import { getRevisions } from '../../../services/rules';
 import { CONTENT_MAX_WIDTH } from '../../../utils/constants';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   radioCell: {
     paddingLeft: 0,
   },
   ruleCard: {
     boxShadow: 'unset',
   },
-  tableWrapper: {
-    maxHeight: '50%',
-    overflowY: 'auto',
-  },
-  tableHeadCell: {
-    position: 'sticky',
-    top: 0,
-    backgroundColor: theme.palette.common.white,
-    zIndex: theme.zIndex.appBar,
-  },
   drawerPaper: {
     maxWidth: CONTENT_MAX_WIDTH,
     margin: '0 auto',
   },
-}));
+});
 
 function ListRuleRevisions(props) {
   const classes = useStyles();
@@ -53,7 +42,7 @@ function ListRuleRevisions(props) {
   const revisions = fetchedRevisions.data
     ? fetchedRevisions.data.data.rules
     : [];
-  const headers = ['Revision Date', 'Changed By', 'Compare', ''];
+  const revisionsCount = revisions.length;
 
   useEffect(() => {
     fetchRevisions(ruleId);
@@ -84,36 +73,6 @@ function ListRuleRevisions(props) {
 
   // TODO: Add logic to restore a revision
   const handleRestoreClick = () => {};
-  const renderRow = (row, index) => (
-    <TableRow key={row.timestamp}>
-      <TableCell title={new Date(row.timestamp)}>
-        {formatDistanceStrict(new Date(row.timestamp), new Date(), {
-          addSuffix: true,
-        })}
-      </TableCell>
-      <TableCell>{row.changed_by}</TableCell>
-      <TableCell className={classes.radioCell}>
-        <Radio
-          variant="red"
-          value={index}
-          disabled={index === 0}
-          checked={leftRadioCheckedIndex === index}
-          onChange={handleLeftRadioChange}
-        />
-        <Radio
-          variant="green"
-          value={index}
-          disabled={index === revisions.length - 1}
-          checked={rightRadioCheckedIndex === index}
-          onChange={handleRightRadioChange}
-        />
-      </TableCell>
-      <TableCell>
-        <Button onClick={handleViewClick(row)}>View</Button>
-        {index > 0 && <Button onClick={handleRestoreClick}>Restore</Button>}
-      </TableCell>
-    </TableRow>
-  );
 
   return (
     <Dashboard title={`Rule ${ruleId} Revisions`}>
@@ -122,17 +81,58 @@ function ListRuleRevisions(props) {
       {!isLoading && revisions.length === 1 && (
         <Typography>Rule {ruleId} has no revisions</Typography>
       )}
-      {!isLoading && revisions.length > 1 && (
+      {!isLoading && revisionsCount > 1 && (
         <Fragment>
-          <div className={classes.tableWrapper}>
-            <DataTable
-              size="small"
-              headers={headers}
-              renderRow={renderRow}
-              items={revisions}
-              tableHeadCellProps={{ className: classes.tableHeadCell }}
+          <RevisionsTable
+            rowCount={revisionsCount}
+            rowGetter={({ index }) => revisions[index]}>
+            <Column
+              label="Revision Date"
+              dataKey="timestamp"
+              cellRenderer={({ cellData }) =>
+                formatDistanceStrict(new Date(cellData), new Date(), {
+                  addSuffix: true,
+                })
+              }
+              width={250}
             />
-          </div>
+            <Column width={250} label="Changed By" dataKey="changed_by" />
+            <Column
+              label="Compare"
+              dataKey="compare"
+              width={250}
+              cellRenderer={({ rowIndex }) => (
+                <Fragment>
+                  <Radio
+                    variant="red"
+                    value={rowIndex}
+                    disabled={rowIndex === 0}
+                    checked={leftRadioCheckedIndex === rowIndex}
+                    onChange={handleLeftRadioChange}
+                  />
+                  <Radio
+                    variant="green"
+                    value={rowIndex}
+                    disabled={rowIndex === revisions.length - 1}
+                    checked={rightRadioCheckedIndex === rowIndex}
+                    onChange={handleRightRadioChange}
+                  />
+                </Fragment>
+              )}
+            />
+            <Column
+              dataKey="actions"
+              width={250}
+              cellRenderer={({ rowData, rowIndex }) => (
+                <Fragment>
+                  <Button onClick={handleViewClick(rowData)}>View</Button>
+                  {rowIndex > 0 && (
+                    <Button onClick={handleRestoreClick}>Restore</Button>
+                  )}
+                </Fragment>
+              )}
+            />
+          </RevisionsTable>
           <br />
           <br />
           {revisions[leftRadioCheckedIndex] &&
