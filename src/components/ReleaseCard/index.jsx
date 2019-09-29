d8dec60b016ca4d470277c2d404daf83fce8f17f
Hassan Ali <helfi92@gmail.com>
2019-09-29 13:33:41 -0400

    Make some buttons disabled when logged out

--- src/components/ReleaseCard/index.jsx
+++ src/components/ReleaseCard/index.jsx
@@ -300,7 +300,7 @@ function ReleaseCard(props) {
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Link className={classes.link} to={`/releases/${release.name}`}>
-          <Button color="secondary">
+          <Button color="secondary" disabled={!user}>
             {release.read_only ? 'View' : 'Update'}
           </Button>
         </Link>
